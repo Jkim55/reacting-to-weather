fe7e4cfd0e783566bce583f0dfce009738aef821
Ji Kim <jkim55@gmail.com>
2017-06-25 17:02:05 -0600

    add react and stubbing with a counter

--- readme.md
+++ readme.md
@@ -3,9 +3,18 @@ Weather forecasting app created to learn more redux
 
 ### Getting Started
 1. Clone down this repo.
-2. Start the app with
+2. Set up the app
+```
+  > npm install
+```
+3. Compile the app
+```
+  > npm run compile
+```
 
+
+4. Start the dev server:
 ```
-	> npm install
-	> npm start
+npm start
 ```
+App runs locally at port 3000.
---
+++ src/counter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+ 
+/**
+ * A counter button: tap the button to increase the count.
+ */
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+ 
+  render() {
+    return (
+      <button
+        onClick={() => {
+          this.setState({ count: this.state.count + 1 });
+        }}
+      >
+        Count: {this.state.count}
+      </button>
+    );
+  }
+}
+export default Counter;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+  );
+});
